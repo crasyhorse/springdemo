95690533e867a6c87d2897527677a4961f540c5d
Florian Weidinger <crasyhorse@gmx.net>
2023-02-18 11:53:35 +0000

    2

--- app/src/main/java/spring/BasicApp.java
+++ app/src/main/java/spring/BasicApp.java
@@ -3,7 +3,6 @@ package spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-
 import spring.basic.BinarySearchImpl;
 
 @SpringBootApplication
