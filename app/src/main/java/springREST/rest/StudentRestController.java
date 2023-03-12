b3b361edcabfaf34522a238d26989ab5f23c51b5
Crasy Horse <crasyhorse@gmx.net>
2023-03-12 19:07:57 +0000

    Moved old projects to /src/main/old

--- app/src/main/java/springREST/rest/StudentRestController.java
+++ app/src/main/java/springREST/rest/StudentRestController.java
@@ -2,14 +2,14 @@ package springREST.rest;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.annotation.PostConstruct;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import springREST.entity.Student;
 
 @RestController
@@ -36,8 +36,7 @@ public class StudentRestController {
     public Student getStudent(@PathVariable int studentId) {
         if (studentId >= students.size() || (studentId < 0)) {
             throw new StudentNotFoundException(
-                String.format("Student with id %s is not found", studentId)
-            );
+                    String.format("Student with id %s is not found", studentId));
         }
         return students.get(studentId);
     }
