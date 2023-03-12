1f06f1f51885dd3a2a1fd2c5b024ed63e703e540
root <crasyhorse@gmx.net>
2023-03-12 18:13:13 +0000

    Finished intro into REST

--- app/src/main/java/springREST/rest/StudentRestController.java
+++ app/src/main/java/springREST/rest/StudentRestController.java
@@ -2,9 +2,10 @@ package springREST.rest;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.annotation.PostConstruct;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +34,11 @@ public class StudentRestController {
 
     @GetMapping("/student/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
+        if (studentId >= students.size() || (studentId < 0)) {
+            throw new StudentNotFoundException(
+                String.format("Student with id %s is not found", studentId)
+            );
+        }
         return students.get(studentId);
     }
 }
